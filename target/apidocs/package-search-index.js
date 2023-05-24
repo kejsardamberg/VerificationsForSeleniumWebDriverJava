957c9e6bc8da38e851c1e11f3c514813836b1e7a
Jörgen Damberg <kejsardamberg@hotmail.com>
2023-05-24 22:11:46 +0200

    Delete target/apidocs directory

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.zingtongroup.verificationsforseleniumwebdriver"},{"l":"com.zingtongroup.verificationsforseleniumwebdriver.verifyingwebdrivercomponents"},{"l":"com.zingtongroup.verificationsforseleniumwebdriver.verifyingwebdrivercomponents.verifications"}];updateSearchResults();
